58ee1035407443348317138afdb87eb857e01867
juan-castro-1 <juan_castro_96@hotmail.com>
2021-04-23 14:16:45 -0300

    added METRICS to .py

--- GetData.py
+++ GetData.py
@@ -35,4 +35,45 @@ CASH_FLOW.to_excel(writer, sheet_name='Cash_Flow')
 INCOME.to_excel(writer, sheet_name='Income')
 # save the excel
 writer.save()
-print('DataFrame is written successfully to Excel File.')
+print('DATA was written successfully to Excel File.')
+
+
+import pandas as pd
+
+df_CF = pd.read_excel(writer, sheet_name='Cash_Flow')
+df_BAL = pd.read_excel(writer, sheet_name='Balance')
+df_INC = pd.read_excel(writer, sheet_name='Income')
+
+METRICS = pd.DataFrame()
+METRICS['fiscal_year'] = df_CF['Fiscal Year']
+METRICS['FCF'] = df_INC['Operating Income (Loss)'] + df_CF['Depreciation & Amortization' ] + df_CF['Change in Fixed Assets & Intangibles' ] + df_CF['Change in Working Capital' ] + df_INC['Income Tax (Expense) Benefit, Net']
+METRICS['FCFF'] = df_CF['Net Income/Starting Line'] + df_CF['Change in Working Capital' ] + df_CF['Change in Fixed Assets & Intangibles' ]
+METRICS['reinvestment_rate'] =  -( df_CF['Change in Working Capital' ] + df_CF['Change in Fixed Assets & Intangibles' ] ) / df_CF['Net Cash from Operating Activities']
+METRICS['ROE'] = df_INC['Net Income (Common)'] / df_BAL['Total Equity']
+METRICS['retention_ratio'] = (df_CF['Net Income/Starting Line'] + df_CF['Dividends Paid']) / df_CF['Net Income/Starting Line']
+METRICS['growth_1'] = METRICS['reinvestment_rate']*METRICS['ROE']
+METRICS['growth_2'] = METRICS['retention_ratio']*METRICS['ROE']
+
+
+
+writer_METRICS = pd.ExcelWriter(fr'{directory}\METRICS.xls')
+METRICS.to_excel(writer_METRICS)
+writer_METRICS.save()
+print('METRICS were written successfully to Excel File.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
